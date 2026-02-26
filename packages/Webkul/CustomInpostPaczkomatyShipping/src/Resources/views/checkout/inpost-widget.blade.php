9b6d138cfac4d7156f3b6b095b151f0f28e4f728
Ola <aleksandra.reja19@gmail.com>
2026-02-26 12:21:45 +0100

    InpostPaczkomaty module added

--- packages/Webkul/CustomInpostPaczkomatyShipping/src/Resources/views/checkout/inpost-widget.blade.php
+++ packages/Webkul/CustomInpostPaczkomatyShipping/src/Resources/views/checkout/inpost-widget.blade.php
@@ -1,9 +1,10 @@
 <div
-    v-if="isInpostSelected"
+    v-if="inpostSelected"
     class="mt-6"
 >
-    <div ref="geowidgetContainer" style="height:400px;"></div>
-    <p class="mt-3 text-sm font-medium text-zinc-500">Wybierz paczkomat</p>
+    <div ref="geowidgetContainer" style="height:400px;">
+        <p class="mt-3 text-sm font-medium text-zinc-500">Wybierz paczkomat</p>
+    </div>
 
     <div v-if="selectedLocker" class="mt-3 p-3 bg-green-600 text-white rounded">
         Wybrano: @{{ selectedLocker }}
