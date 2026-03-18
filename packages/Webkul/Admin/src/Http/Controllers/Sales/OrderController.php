637c53c0ae1e2fbdbefcffaa7f7c3e37505d237a
Ola <aleksandra.reja19@gmail.com>
2026-03-18 10:25:41 +0100

    Errors fixed

--- packages/Webkul/Admin/src/Http/Controllers/Sales/OrderController.php
+++ packages/Webkul/Admin/src/Http/Controllers/Sales/OrderController.php
@@ -121,14 +121,13 @@ class OrderController extends Controller
      */
     public function view(int $id)
     {
-        $order = $this->orderRepository->findOrFail($id);
-        $order->load([
+        $order = $this->orderRepository->with([
             'customer',
             'items',
             'billing_address',
             'shipping_address',
             'payment',
-        ]);
+        ])->findOrFail($id);
 
         return view('admin::sales.orders.view', compact('order'));
     }
